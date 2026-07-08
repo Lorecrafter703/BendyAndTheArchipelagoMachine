e090ff59b5587cb5ca9d0f7bd83cbe855592bcbf
Daniel <Daniel@MY-PC-TM>
2026-07-07 19:04:30 -0500

    Add project files.

---
+++ BendyAndTheArchipelagoMachine/BendyAndTheArchipelagoMachine.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BendyAndTheArchipelagoMachine
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BendyAndTheArchipelagoMachine : BaseUnityPlugin
+    {
+        public const string pluginGuid = "lorecrafter.bendyandtheinkmachine.archipelago";
+        public const string pluginName = "Bendy and the Archipelago Machine";
+        public const string pluginVersion = "1.0.0.0";
+
+        internal static new ManualLogSource Logger;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogMessage("Hello World");
+
+            Harmony harmony = new Harmony(pluginGuid);
+        }
+    }
+}
